4e1212fe8ba7c87bcd4a2fa43a2573f542018029
Aswin Prakash <aswinprakash18@gmail.com>
2015-05-06 13:48:06 -0700

    first commit

--- server.js
+++ server.js
@@ -12,10 +12,6 @@ app.get('/',function(req,res){
 	res.render('index');
 });
 
-app.get('/board',function(req,res){
-	res.render('board');
-});
-
 var server = app.listen(8000,function(){
 	console.log('listening to port 8000');
 });
@@ -23,20 +19,42 @@ var server = app.listen(8000,function(){
 
 var users={};
 var msgs=[];
+var chatrooms={};
+var chatroom_list=[];
 
 var io = require('socket.io').listen(server);
 io.sockets.on('connection',function(socket){
-	// Listen to new user data, add info to users, and give new user all existing message
+	// Listen to new user data, store user's info and emit chatroom_list data
 	socket.on('user_name',function(data){
 		users[socket.id]=data;
-		socket.emit('board_data',msgs);
+		socket.emit('chatroom_list',chatroom_list)
+	});
+	// Listen to chatroom data, store chatroom data,sends user chatroom data
+	socket.on('select_chatroom',function(data){
+		if (data.new) { //if new chatroom, create new JSON item and add to list of chatrooms
+			chatrooms[data.chatroom]={
+				'chatroom_id':data.chatroom,
+				'msgs':[]
+			};
+			chatroom_list.push(data.chatroom);
+		}
+		socket.emit('board_data',chatrooms[data.chatroom]);
+		// console.log(chatrooms);
 	});
 	// Listen to new message by any user, save it to message list, then broadcast latest
 	socket.on('msg_data',function(data){
-		var msg={'name':users[socket.id],'msg':data};
-		// msg[users[socket.id]]=data;
-		msgs.push(msg);
+		var msg={
+			'chatroom_id':data.chatroom_id,
+			'name':users[socket.id],
+			'msg':data.msg
+		};
+		chatrooms[data.chatroom_id].msgs.push(msg);
 		io.emit("new_msg",msg);
-		console.log(msg);
+		// console.log(msg);
+		// console.log(chatrooms);
+	});
+	//Listen to user exiting room and give user updated list of rooms (to previous created socket listener)
+	socket.on('exit_room',function(data){
+		socket.emit('chatroom_list',chatroom_list);
 	});
 });
